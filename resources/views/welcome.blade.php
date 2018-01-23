b908064e97a704daa842c26b39e091850c2e2aff
Anders Jürisoo <jurisoo@hotmail.com>
2018-01-23 16:41:06 +0100

    Added temporary favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,8 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+        <link rel="icon" href="img/favicon.ico" type="image/x-icon">
         <title>Laravel Stimpack</title>
         <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css">
         <script src="http://ajaxorg.github.io/ace-builds/src-noconflict/ace.js" type="text/javascript" charset="utf-8"></script>
