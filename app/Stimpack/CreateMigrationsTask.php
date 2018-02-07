715a1a61734a85e0bfa7836053306535dae6f355
Anders Jürisoo <jurisoo@hotmail.com>
2018-02-07 12:12:15 +0100

    Fixed relative migration task

--- app/Stimpack/CreateMigrationsTask.php
+++ app/Stimpack/CreateMigrationsTask.php
@@ -3,7 +3,7 @@
 namespace App\Stimpack;
 use Illuminate\Support\Facades\Log;
 
-class CreateMigrationsTask implements Task
+class CreateMigrationsTask extends Task
 {
 
     public function __construct($tasks) {
@@ -14,8 +14,8 @@ class CreateMigrationsTask implements Task
         Log::info(json_encode($this->tasks->CreateMigrationsTask));
         $message = "";
         foreach($this->tasks->CreateMigrationsTask->migrations as $migration) {
-            $migrationName = date("Y_m_d_hms",time()) . "_create_" . $migration->table . "_table.php";
-            $path = database_path("migrations/" . $migrationName);
+            $migrationName = date("Y_m_d_hms",time()) . "_create_" . $migration->table . "_table.php";            
+            $path = $this->projectPath() . "/database/migrations/" . $migrationName;
             file_put_contents($path, $migration->body);                
             $message = $message . "Created migration at '" . $path . "'";
         }
