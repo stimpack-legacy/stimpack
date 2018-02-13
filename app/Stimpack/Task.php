4a80e8e4e32ab9f8b308c6b7ecede3ac6bf8f74c
Anders Jürisoo <jurisoo@hotmail.com>
2018-02-13 21:47:44 +0100

    Better access to Task properties

--- app/Stimpack/Task.php
+++ app/Stimpack/Task.php
@@ -10,36 +10,30 @@ class Task
     }
 
     public function projectPath() {
-        if($this->get("SetTargetProjectTask") && $this->get("SetTargetProjectTask")->enabled) {
-            return base_path() . $this->get("SetTargetProjectTask")->projectPath . $this->get("SetTargetProjectTask")->projectName;
+        if($this->SetTargetProjectTask && $this->SetTargetProjectTask->enabled) {
+            return base_path() . $this->SetTargetProjectTask->projectPath . $this->SetTargetProjectTask->projectName;
         }
 
         return base_path();
     }
-
-    public function get($taskName) {
-        return $this->tasks->first(function($value) use($taskName) {
-            return $value->taskName == $taskName;
-        });
-    }
-
-    /*
-    / Shorthand for accessing the current tasks properties
-    / Example:
-    / Instead of 
-    / $this->tasks->CreateControllersTask->enabled
-    / Use
-    / $this->enabled
-    */
 
     private function transferParameters() {
         $taskClassName = class_basename(get_class($this));
         $thisTask = $this->tasks->first(function($value) use($taskClassName) {
             return $value->taskName == $taskClassName;
         });
+
+        // Transfer the current task directly onto this
         foreach ($thisTask as $key => $value) {
             $this->$key = $value;
         }
+
+        // Transfer all task to this so they can be called by name
+        foreach ($this->tasks as $key => $value) {
+            $taskName = $value->taskName;
+            $this->$taskName = $value;
+        }
+
     }
 
     public static function projects() {
