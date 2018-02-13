1872e33decd47ca52f8546277bf70bdf99cb5f4d
Anders Jürisoo <jurisoo@hotmail.com>
2018-02-13 12:31:56 +0100

    Cleanup

--- public/js/app.js
+++ public/js/app.js
@@ -17739,26 +17739,6 @@ var Main = function (_Component) {
     return Main;
 }(__WEBPACK_IMPORTED_MODULE_0_react__["Component"]);
 
-/*
-<Generator />
-
-                <Header />
-                <Generator />
-            <div className="container">
-                <div className="card">
-                <div className="card-header">
-                    Featured
-                </div>
-                <div className="card-body">
-                    <h5 className="card-title">Special title treatment</h5>
-                    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
-                </div>
-                </div>                
-            </div>                
-*/
-
-
 /* harmony default export */ __webpack_exports__["default"] = (Main);
 
 /***/ }),
--- resources/assets/js/components/Main.js
+++ resources/assets/js/components/Main.js
@@ -13,23 +13,3 @@ export default class Main extends Component {
         );
     }
 }
-
-
-/*
-<Generator />
-
-                <Header />
-                <Generator />
-            <div className="container">
-                <div className="card">
-                <div className="card-header">
-                    Featured
-                </div>
-                <div className="card-body">
-                    <h5 className="card-title">Special title treatment</h5>
-                    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
-                </div>
-                </div>                
-            </div>                
-*/
