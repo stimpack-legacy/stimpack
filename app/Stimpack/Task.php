52df349d160a45dec295cc073a9aa3cd236a8f34
Anders Jürisoo <jurisoo@hotmail.com>
2018-02-15 15:59:57 +0100

    Convention can be ugly

--- app/Stimpack/Task.php
+++ app/Stimpack/Task.php
@@ -4,27 +4,32 @@ namespace App\Stimpack;
 
 class Task
 {
-    public function __construct($tasks) {
+    public function __construct($tasks)
+    {
         $this->tasks = collect($tasks);
         $this->transferParameters();        
     }
 
-    public function projectPath() {
-        if(isset($this->SetTargetProjectTask) && $this->SetTargetProjectTask->enabled) {
+    public function projectPath()
+    {
+        if(isset($this->SetTargetProjectTask))
+        {
             return base_path() . $this->SetTargetProjectTask->projectPath . $this->SetTargetProjectTask->projectName;
         }
 
         return base_path();
     }
 
-    private function transferParameters() {
+    private function transferParameters()
+    {
         $taskClassName = class_basename(get_class($this));
         $thisTask = $this->tasks->first(function($value) use($taskClassName) {
             return $value->taskName == $taskClassName;
         });
 
         // Transfer the current task directly onto this
-        foreach ($thisTask as $key => $value) {
+        foreach ($thisTask as $key => $value)
+        {
             $this->$key = $value;
         }
 
