b2b6ecf453df818b900d6d87f01f407cffed4570
Anders Jürisoo <jurisoo@hotmail.com>
2018-03-08 15:03:57 +0100

    Cleaned up migration naming

--- app/Stimpack/Tasks/CreateMigrationsTask.php
+++ app/Stimpack/Tasks/CreateMigrationsTask.php
@@ -18,6 +18,7 @@ class CreateMigrationsTask extends Task
         // Place new files
         foreach($this->files as $key=>$file) {        
             $fileName = str_pad(($key+1), 3, "0", STR_PAD_LEFT) . "_create_" . $file->table . "_table.php";            
+            $fileName = (1000 + $key+1) . "____create_" . $file->table . "_table.php";            
             $path = $this->projectPath() . "/database/migrations/" . $fileName;
             file_put_contents($path, $file->body);                
             $message = $message . "Created migration at '" . $path . "'";
