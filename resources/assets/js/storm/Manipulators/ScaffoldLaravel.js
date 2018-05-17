97dcd398a61ce1c625072b5080eb29f3393b7cfb
Anders Jürisoo <jurisoo@hotmail.com>
2018-05-17 21:14:01 +0200

    Added tabs

--- resources/assets/js/storm/Manipulators/ScaffoldLaravel.js
+++ resources/assets/js/storm/Manipulators/ScaffoldLaravel.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import {connect} from 'react-redux';
 import BaseManipulator from "../BaseManipulator";
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 class ScaffoldLaravel extends BaseManipulator {
     
@@ -13,11 +14,38 @@ class ScaffoldLaravel extends BaseManipulator {
 
     renderSettings() {        
         return (
-            <div className="container">
-                <h4>Object pseudo model - Stubs - Settings - Preview/Override</h4>                
-                <div className="form-group code-text-area">
-                    <textarea rows="20" name="pseudoCode" placeholder="Some Code Here..." value={this.state.data.pseudoCode} type="text" className="form-control" onChange={this.setDataParameter.bind(this)} />
-                </div>                
+            <div className="container">                
+                <Tabs>
+                    <TabList>
+                    <Tab><h4>PseudoCode</h4></Tab>
+                    <Tab><h4>Stubs</h4></Tab>
+                    <Tab><h4>Settings</h4></Tab>
+                    <Tab><h4>Result</h4></Tab>
+                    </TabList>
+
+                    <TabPanel>
+                        <div className="form-group code-text-area">
+                            <textarea rows="20" name="pseudoCode" placeholder="Some Code Here..." value={this.state.data.pseudoCode} type="text" className="form-control" onChange={this.setDataParameter.bind(this)} />
+                        </div>
+                    </TabPanel>
+                    <TabPanel>
+                        <div className="form-group code-text-area">
+                            <textarea rows="20" name="pseudoCode" placeholder="Some Code Here..." value={this.state.data.pseudoCode} type="text" className="form-control" onChange={this.setDataParameter.bind(this)} />
+                        </div>
+                    </TabPanel>
+                    <TabPanel>
+                        <p> This is some settings for you to change </p>
+                    </TabPanel>
+                    <TabPanel>
+                    <div className="form-group code-text-area">
+                            <textarea rows="20" name="pseudoCode" placeholder="Some Code Here..." value={this.state.data.pseudoCode} type="text" className="form-control" onChange={this.setDataParameter.bind(this)} />
+                        </div>                        
+                    </TabPanel>
+                </Tabs>
+                    
+                
+
+                                
             </div>
         );
     }   
