ebf0e66db972b39674ebc72edac9088d3fbc3313
Anders Jürisoo <jurisoo@hotmail.com>
2018-05-24 12:50:36 +0200

    It sets up database automatically

--- app/Console/Commands/ParkCommand.php
+++ app/Console/Commands/ParkCommand.php
@@ -6,6 +6,7 @@ use App\Console\Commands\StimpackCommand;
 use Illuminate\Console\Command;
 use App\Console\Controllers\ManipulatorController;
 use App\Http\Controllers\GuiController;
+use Config;
 
 
 class ParkCommand extends StimpackCommand
@@ -42,12 +43,27 @@ class ParkCommand extends StimpackCommand
     public function setupDatabase()
     {
         $dbPath = base_path('storage/stimpack/stimpack.sqlite');
-        if(!file_exists($dbPath)) {
-            $this->info("Created a new database at " . $dbPath);
-        }
-        
         set_env_value("DB_DATABASE", base_path('storage/stimpack/stimpack.sqlite'));
+
+        if(!file_exists($dbPath)) {
+            touch($dbPath);
+            $this->info("Created a new database at " . $dbPath);
+            
+            Config::set("database.connections.stimpack", [
+                "driver" => "sqlite",
+                "database" => $dbPath
+            ]);
+            
+            $this->call('migrate', [
+                '--database' => 'stimpack',
+                '--force' => true
+            ]);
+
+            $this->info("Migrated database");
+        }
     }
+
+
 
     protected function createSymlinkToCode()
     {
